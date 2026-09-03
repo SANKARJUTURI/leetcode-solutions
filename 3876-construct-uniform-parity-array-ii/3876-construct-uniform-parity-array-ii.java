0dc09108ed8a9bc1743274fb22f107ad00771e74
JUTURI LAKSHMI ANANTHA SANKAR <juturisankar2@gmail.com>
2026-09-03 08:16:37 +0530

    Time: 5 ms (83.33%), Space: 121.8 MB (30.44%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public boolean uniformArray(int[] nums1) 
+    {
+        int min=Integer.MAX_VALUE;
+        boolean hasOdd=false;
+        boolean hasEven=false;
+        for(int x:nums1) 
+        {
+            min=Math.min(min,x);
+            if(x%2==0)hasEven=true;
+            else hasOdd=true;
+        }
+        if(!hasOdd || !hasEven)return true;
+        return min%2!=0;
+    }
+}
