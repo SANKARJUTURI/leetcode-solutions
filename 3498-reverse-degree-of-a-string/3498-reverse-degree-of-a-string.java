ee1d1e7a14f2e896b7500fd35ba5d76e1fedab20
JUTURI LAKSHMI ANANTHA SANKAR <juturisankar2@gmail.com>
2026-09-20 09:10:04 +0530

    Time: 1 ms (100%), Space: 44.2 MB (34.29%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int reverseDegree(String s) 
+    {
+        int n=s.length();
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=(26-(s.charAt(i)-'a'));
+            res+=(x*(i+1));
+        }
+        return res;
+    }
+}
